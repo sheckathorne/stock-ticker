4d6d92c212eb4526090285241b438f06bcb70c80
Sean Heckathorne <heckathorne.s@gmail.com>
2023-04-05 21:33:23 -0500

    refactored some ticker code

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,16 @@
 use exitfailure::ExitFailure;
-use reqwest::Url;
+use reqwest::{Url, Client};
 use serde_derive::{Deserialize, Serialize};
 use std::env;
 use tokio::time::{sleep, Duration};
 use dotenv::dotenv;
+
+struct App {
+    api_key: String,
+    client: Client,
+    quote_history: Vec<CompanyQuote>
+}
+
 
 #[derive(Serialize, Deserialize, Debug)]
 struct CompanyQuote {
@@ -16,31 +23,39 @@ struct CompanyQuote {
 }
 
 impl CompanyQuote {
-    async fn get(symbol: &String, api_key: &String, quote_history: &mut Vec<CompanyQuote>) -> Result<(), ExitFailure> {
+    async fn get(symbol: &String, config: &mut App) -> Result<(), ExitFailure> {
         let url = format!(
             "https://finnhub.io/api/v1/quote?symbol={}&token={}",
-            symbol, api_key
+            symbol, config.api_key
         );
 
         let url = Url::parse(&url)?;
-        let res = reqwest::get(url).await?.json::<CompanyQuote>().await?;
-        quote_history.push(res);
+        let res = config.client.get(url).send().await?.json::<CompanyQuote>().await?;
+        config.quote_history.push(res);
 
         Ok(())
     }
 }
 
+fn init_config() -> App {
+    dotenv().ok();
+    App {
+        api_key: std::env::var("API_KEY").expect("API key must be set in .env").to_string(),
+        client: reqwest::Client::new(),
+        quote_history: vec![]
+    }
+}
+
 #[tokio::main]
 async fn main() -> Result<(), ExitFailure> {
-    dotenv().ok();
-    let api_key = std::env::var("API_KEY").expect("API key must be set in .env").to_string();
+    let mut config = init_config();
     let args: Vec<String> = env::args().collect();
-    let symbol: String = args.get(1).unwrap_or(&"AMC".to_string()).to_string();
-    let mut quote_history: Vec<CompanyQuote> = vec![];
+    let symbol: String = args.get(1).unwrap_or(&"AAPL".to_string()).to_string();
+
 
     loop {
-        CompanyQuote::get(&symbol, &api_key, &mut quote_history).await?;
-        let latest_quote = quote_history.last().unwrap();
+        CompanyQuote::get(&symbol, &mut config).await?;
+        let latest_quote = config.quote_history.last().unwrap();
         println!("{}'s current stock price: {} at time {}", symbol, latest_quote.c, latest_quote.t);
         sleep(Duration::from_millis(5_000)).await;
     }
